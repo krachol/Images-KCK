a9b198bf1353d2ab756f87a074263b6f48dc6ff2
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-11-09 16:13:53 +0100

    Testing PyCharm GitHub integration

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def main() -> null:
+def main():
     x = np.array([1, 2, 3, 4, 5])
-    y = x * 20
+    y = x * 30
 
     plt.plot(x, y)
     plt.show()
