f4dc5e33dc03bf928eb6202e5eb8574908ffeedc
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-11-10 15:58:00 +0100

    Add simple multiimage setup

--- Main.py
+++ Main.py
@@ -1,17 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import data
-from skimage.color import rgb2hsv, hsv2rgb, rgb2gray
+from skimage.color import rgb2hsv, hsv2rgb,
 
 def main():
     image = data.chelsea()
 
+    fig = plt.figure()
+    a = fig.add_subplot(2,1,1)
+
+    plt.imshow(image)
+
+    a = fig.add_subplot(2,1,2)
+
     image = rgb2hsv(image)
     image[:,:,1] = 0
     image = hsv2rgb(image)
-
     plt.imshow(image)
-    plt.savefig('my_image.jpg')
+
+
+    fig.savefig('my_image.jpg')
 
 if __name__ == "__main__":
     main()
