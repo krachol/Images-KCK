11b349d2789a5daf00dff771f50e35c45f520306
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-11-10 18:59:22 +0100

    Construct basic transformation framework for images in a folder

--- Main.py
+++ Main.py
@@ -1,25 +1,49 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import os
 from skimage import data
-from skimage.color import rgb2hsv, hsv2rgb,
+
+IMAGE_DIR = 'Images/'
+OUTPUT_FILENAME = 'result.jpg'
+ROW_NUM = 2
+
+
+def get_images():
+    images_filenames = os.listdir(IMAGE_DIR)
+    images = []
+
+    for image_filename in images_filenames:
+        abs_image_filename = os.path.abspath(os.path.join(IMAGE_DIR, image_filename))
+        images.append(data.imread(abs_image_filename, as_grey=True))
+
+    return images
+
+
+def plot_images(images):
+    fig = plt.figure(num=None, figsize=(40, 40), dpi=80)
+
+    for index, image in enumerate(images):
+        a = fig.add_subplot(5, 5, index + 1)
+        plt.imshow(image, cmap="Greys_r")
+        a.set_title(str(index))
+        plt.axis("off")
+
+    fig.savefig(OUTPUT_FILENAME)
+
+
+def transform_image(image):
+    after_transform = image
+    return after_transform
+
+
+def transform(images):
+    for index, image in enumerate(images):
+        images[index] = transform_image(image)
+    return images
+
 
 def main():
-    image = data.chelsea()
+    plot_images(transform(get_images()))
 
-    fig = plt.figure()
-    a = fig.add_subplot(2,1,1)
-
-    plt.imshow(image)
-
-    a = fig.add_subplot(2,1,2)
-
-    image = rgb2hsv(image)
-    image[:,:,1] = 0
-    image = hsv2rgb(image)
-    plt.imshow(image)
-
-
-    fig.savefig('my_image.jpg')
 
 if __name__ == "__main__":
     main()
