9f9162a5f03c7c7809a84df87c61d86196c8e035
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-11-12 22:26:26 +0100

    Change functions that return arrays to generators

--- Main.py
+++ Main.py
@@ -19,38 +19,40 @@ def get_images(image_dir):
     :return: array of raw image data
     """
 
-    images_filenames = os.listdir(image_dir)
-    images = []
-
-    for image_filename in images_filenames:
+    for image_filename in os.listdir(image_dir):
         # Get absolute filenames
         abs_image_filename = os.path.abspath(os.path.join(IMAGE_DIR, image_filename))
         # Read in images from list to images table
-        images.append(data.imread(abs_image_filename, as_grey=False))
-
-    # return images table
-    return images
+        yield data.imread(abs_image_filename, as_grey=False)
 
 
-def plot_images(images, output_filename, rows=5, columns=5):
+def get(i):
+    return i
+
+
+def get_next_filename(output_filename):
+    return output_filename
+
+
+def plot_images(images, output_filename, rows=6, columns=4):
     """
     Draw images in a mosaic and save to output filename
     :param columns: number of columns in mosaic (default 5)
     :param rows: number of rows in mosaic (default 5)
-    :param images: array of images to contain in a mosaic
+    :param images_gen: generator of an array of images to contain in a mosaic
     :param output_filename: name of the file to save mosaic into
     """
 
-    fig = plt.figure(num=None, figsize=(30, 30), dpi=80)
+    fig = plt.figure(num=None, figsize=(40, 40), dpi=80)
 
     for index, image in enumerate(images):
         a = fig.add_subplot(rows, columns, index + 1)
-
-        plt.imshow(image, cmap=plt.cm.gray)
-        a.set_title(str(index))
+        plt.imshow(image)
         plt.axis("off")
 
-    fig.savefig(output_filename)
+    fig.tight_layout()
+    fig.savefig(get_next_filename(output_filename))
+    plt.close('all')
 
 
 def transform_image(image):
@@ -64,17 +66,17 @@ def transform_image(image):
     # median_transform = median(before_transform, disk(10))
     elevation_map = sobel(transformable_image)
     markers = np.ones_like(transformable_image)
-    markers[transformable_image < 0.4] = 2
-    markers[transformable_image > 0.7] = 0
+    markers[transformable_image < 0.2] = 2
+    markers[transformable_image > 0.5] = 0
 
     transformable_image = morphology.watershed(elevation_map, markers)
 
-    segmentation = ndi.binary_fill_holes(transformable_image -1)
-    #labeled, _ = ndi.label(segmentation)
+    segmentation = ndi.binary_fill_holes(transformable_image - 1)
+    labeled, _ = ndi.label(segmentation)
 
-    #image_overlay = label2rgb(labeled, image=image)
+    image_overlay = label2rgb(labeled, image=image, bg_label=0)
 
-    return segmentation
+    return image_overlay
 
 
 def transform(images):
@@ -84,8 +86,7 @@ def transform(images):
     """
 
     for index, image in enumerate(images):
-        images[index] = transform_image(image)
-    return images
+        yield transform_image(image)
 
 
 def main():
